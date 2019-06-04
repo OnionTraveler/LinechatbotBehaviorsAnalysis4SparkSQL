05316b369ad86c25261f148a6d8882048a6d43e3
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-06-04 20:50:08 +0800

    Update memberProfile2df.py

--- analysisProject/memberProfile2df.py
+++ analysisProject/memberProfile2df.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
@@ -9,4 +10,13 @@ if __name__ == "__main__":
     # Create a DataFrame from a JSON file
     memberProfile_df = spark.read.csv("hdfs://localhost/user/cloudera/SparkSQLBehaviorsAnalysis4Linechatbot/data/memberProfile4MySQL", header=True)
 
+
+    
+
+
+#========================= (Launch Spark by 「Interactive Development Environment」)
+# pyspark --master spark://172.21.0.2:7077
+
+#========================= (Launch Spark by 「Shell Script」)
+# spark-submit --master spark://172.21.0.2:7077 message_event_analysis.py
 
